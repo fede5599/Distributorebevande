7b7f7ad8c0bca8356fe16082fb0b7e4cf08e59cd
fede5599 <federicazaccaro@outlook.it>
2024-05-16 12:08:19 +0200

    Distributore mejo

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -15,59 +15,57 @@ public class Main {
 		float[] prezzo = new float[DISTRIBUTORE];
 		int[] quantità = new int[DISTRIBUTORE];
 
-		int i, j=0, inputGest;
+		int i, j = 0, inputGest;
 		float soldi, resto, indiceProdotto = 0, quantitàProdotto = 0;
 		boolean trovato;
-		String daCercare, risposta, codiceinserito="#0000", codiceg= "#0852", rispostaGest= "null";
-		
-		
-		
+		String daCercare, risposta, codiceinserito = "#0000", codiceg = "#0852", rispostaGest = "null";
+
 		System.out.println("BENVENUTO, SELEZIONARE IL PRODOTTO");
-		
+
 		// for per inserire l'elenco prodotti GESTIONALE
-		
+
 		do {
-			System.out.print("Digitare il codice gestore: "); //eliminare
-			codiceinserito=sc.nextLine();
+			System.out.print("Digitare il codice gestore: "); // eliminare
+			codiceinserito = sc.nextLine();
 			if (codiceinserito.equals("#0852")) {
 				System.out.println("Benvenuto gestore");
-				}
-			else { 
+			} else {
 				System.out.println("Codice errato passerai al lato utente");
 				break;
-		}
-			System.out.println("Cosa vuoi fare oggi? "); 
-			int[] numeroMenu = {1, 2, 3, 4, 5, 6, 7};
-			String[] nomiMenu = {"modificaSlot", "incasso", "restoMax", "resetSlot", "qtaVenduta", "disabilita", "reset"};
-			
+			}
+			System.out.println("Cosa vuoi fare oggi? ");
+			int[] numeroMenu = { 1, 2, 3, 4, 5, 6, 7 };
+			String[] nomiMenu = { "modificaSlot", "incasso", "restoMax", "resetSlot", "qtaVenduta", "disabilita",
+					"reset" };
+
 			for (j = 0; j < numeroMenu.length; j++)
-				System.out.println( + numeroMenu[j] + " " + nomiMenu[j]);
+				System.out.println(+numeroMenu[j] + " " + nomiMenu[j]);
 
-		do {
-			System.out.println("Inserisci numero del menù: ");
-			inputGest=sc.nextInt();
-			sc.nextLine();
-			
-		if (inputGest == 1) {
+			do {
+				System.out.println("Inserisci numero del menù: ");
+				inputGest = sc.nextInt();
+				sc.nextLine();
 
-		  for (i = 0; i < codice.length; i++) {
-			System.out.println("Inserisci il codice da dare al prodotto " + i + ": ");
-			codice[i] = sc.nextLine();
+				if (inputGest == 1) {
 
-			System.out.println("Inserisci il prezzo di " + codice[i] + ": ");
-			prezzo[i] = sc.nextFloat();
+					for (i = 0; i < codice.length; i++) {
+						System.out.println("Inserisci il codice da dare al prodotto " + i + ": ");
+						codice[i] = sc.nextLine();
 
-			System.out.println("Inserisci la quantità di " + codice[i] + ": ");
-			quantità[i] = sc.nextInt();
-			sc.nextLine();
-			quantitàProdotto = quantità[i];
-		}
-		}
-		System.out.println("Vuoi tornare al menù? ");
-		rispostaGest=sc.next();
-		} while (!rispostaGest.equals("si"));
-		} while (!codiceinserito.equals("#0852")); 
-		 
+						System.out.println("Inserisci il prezzo di " + codice[i] + ": ");
+						prezzo[i] = sc.nextFloat();
+
+						System.out.println("Inserisci la quantità di " + codice[i] + ": ");
+						quantità[i] = sc.nextInt();
+						sc.nextLine();
+						quantitàProdotto = quantità[i];
+					}
+				}
+				System.out.println("Vuoi tornare al menù? ");
+				rispostaGest = sc.next();
+			} while (!rispostaGest.equals("si"));
+		} while (!codiceinserito.equals("#0852"));
+
 		// parte il ciclo do - while per la selezione dei prodotti
 		do {
 
@@ -81,7 +79,7 @@ public class Main {
 						trovato = true;
 						System.out.println(" Prezzo: " + prezzo[i]);
 						indiceProdotto = prezzo[i];
-					} else if (!trovato)       // trovato ==false
+					} else if (!trovato) // trovato ==false
 						System.out.println("codice non trovato!");
 				}
 
