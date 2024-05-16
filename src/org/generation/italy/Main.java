8c26e68f03a43cd4236e464218c503fec8bfb34a
fede5599 <federicazaccaro@outlook.it>
2024-05-16 13:04:36 +0200

    MODIFICA_1)INSERIMENTO MENÙ A TENDINA 1,2,3;

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -16,13 +16,13 @@ public class Main {
 		int[] quantità = new int[DISTRIBUTORE];
 
 		int i, j = 0, inputGest;
-		float soldi, resto, indiceProdotto = 0, quantitàProdotto = 0;
+		float soldi, resto, indiceProdotto = 0, quantitàProdotto = 0, saldo=100, restoMax=5;
 		boolean trovato;
 		String daCercare, risposta, codiceinserito = "#0000", codiceg = "#0852", rispostaGest = "null";
 
 		System.out.println("BENVENUTO, SELEZIONARE IL PRODOTTO");
 
-		// for per inserire l'elenco prodotti GESTIONALE
+		//INSERIMENTO CODICE GESTORE PER ACCEDERE ALLA PARTE GESTIONALE
 
 		do {
 			System.out.print("Digitare il codice gestore: "); // eliminare
@@ -32,6 +32,8 @@ public class Main {
 			} else {
 				System.out.println("Codice errato passerai al lato utente");
 				break;
+				
+				//APERTURA MENÙ A TENDINA CON LE 7 SCELTE 
 			}
 			System.out.println("Cosa vuoi fare oggi? ");
 			int[] numeroMenu = { 1, 2, 3, 4, 5, 6, 7 };
@@ -41,11 +43,15 @@ public class Main {
 			for (j = 0; j < numeroMenu.length; j++)
 				System.out.println(+numeroMenu[j] + " " + nomiMenu[j]);
 
+			//INSERIMENTO OPZIONE CHE SI VUOLE SCEGLIERE
+			
 			do {
 				System.out.println("Inserisci numero del menù: ");
 				inputGest = sc.nextInt();
 				sc.nextLine();
 
+				//SCELTA 1)MODIFICA SLOT DEL PRODOTTO
+				
 				if (inputGest == 1) {
 
 					for (i = 0; i < codice.length; i++) {
@@ -61,17 +67,49 @@ public class Main {
 						quantitàProdotto = quantità[i];
 					}
 				}
+				
+				// SCELTA 2)PRELEVARE IL SALDO ATTUALE
+				
+				else if (inputGest == 2) {
+					System.out.println("Il saldo totale è: " +saldo +" vuoi prelevarlo?");
+					rispostaGest =sc.next();
+					if(rispostaGest.equalsIgnoreCase("si")) {
+				 System.out.println("Hai prelevato " +saldo);
+					} else {
+				}
+					
+				}
+				
+				// SCELTA 3)MODIFICA IL RESTO MAX
+				
+				
+				else if (inputGest == 3) {
+					System.out.println("Il resto massimo attuale è: " +restoMax +" vuoi modificarlo?");
+					rispostaGest =sc.next();
+					if(rispostaGest.equalsIgnoreCase("si")) {
+				    System.out.println("Inserisci nuovo resto massimo: " );
+					restoMax=sc.nextFloat();
+					sc.nextLine();
+					System.out.println("Il nuovo resto massimo è " +restoMax);
+					} else {
+					}
+				}
+			
+				
+				
+				
+				
 				System.out.println("Vuoi tornare al menù? ");
 				rispostaGest = sc.next();
-			} while (!rispostaGest.equals("si"));
+			} while (rispostaGest.equals("si"));
+			
 		} while (!codiceinserito.equals("#0852"));
 
 		// parte il ciclo do - while per la selezione dei prodotti
-		do {
+		
 
 			do {
 
-				System.out.println("Quale prodotto vuoi acquistare?: ");
 				daCercare = sc.nextLine();
 				trovato = false;
 				for (i = 0; i < codice.length; i++) {
@@ -79,11 +117,9 @@ public class Main {
 						trovato = true;
 						System.out.println(" Prezzo: " + prezzo[i]);
 						indiceProdotto = prezzo[i];
-					} else if (!trovato) // trovato ==false
-						System.out.println("codice non trovato!");
 				}
 
-			} while (trovato == false);
+			} 
 
 			do {
 
@@ -95,6 +131,7 @@ public class Main {
 				if (soldi == indiceProdotto) {
 					System.out.println("Esce il prodotto");
 					quantitàProdotto--;
+					saldo=saldo+indiceProdotto;
 				} else if (soldi > indiceProdotto) {
 					System.out.println("Esce il prodotto con il resto");
 					resto = soldi - indiceProdotto;
